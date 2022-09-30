5857d06ff9c5d785d8b3938bade8ac86acaa2886
Ildar <Ildar.talipov.00@mail.ru>
2022-09-30 09:49:59 +0300

    change dist

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -52,12 +52,12 @@ export default {
       this.list = null;
       if (i === 1) {
         this.loading = true;
-        axios.get('http://localhost:8080/?rows=1000&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&delay=3&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D')
+        axios.get('https://www.filltext.com/?rows=1000&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&delay=3&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D')
             .then(response => this.list = response.data)
             .finally(() => this.loading = false)
       } else {
         this.loading2 = true;
-        axios.get('http://localhost:8080/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D')
+        axios.get('https://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D')
             .then(response => this.list = response.data)
             .finally(() => this.loading2 = false)
       }
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,5 @@
 module.exports = {
   publicPath: '/oxem/',
-  devServer: {
-    proxy: 'https://www.filltext.com/',
-},
 
   transpileDependencies: [
     'vuetify'
